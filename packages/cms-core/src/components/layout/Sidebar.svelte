256db84f72960f31c90e4e86fe8f593a532057fa
Ben <benjaminsinidol@gmail.com>
2025-10-27 23:56:42 +0800

    add h-screen into sidebar

--- packages/cms-core/src/components/layout/Sidebar.svelte
+++ packages/cms-core/src/components/layout/Sidebar.svelte
@@ -24,9 +24,9 @@
 </script>
 
 <ModeWatcher />
-<SidebarProvider>
+<SidebarProvider class="h-screen">
 	<AppSidebar {data} {onSignOut} />
-	<SidebarInset>
+	<SidebarInset class="flex h-full flex-col">
 		<header
 			class="flex h-16 shrink-0 items-center gap-2 border-b transition-[width,height] ease-linear group-has-[[data-collapsible=icon]]/sidebar-wrapper:h-12"
 		>

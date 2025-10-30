6e0d971ba6fe736f57451bcfab803a4c7a527a98
Amumug <fabriali4@gmail.com>
2025-10-30 09:56:47 +0100

    fix import error on ImageField.svelte file

--- packages/cms-core/src/components/admin/fields/ImageField.svelte
+++ packages/cms-core/src/components/admin/fields/ImageField.svelte
@@ -10,7 +10,7 @@
 		DropdownMenuContent,
 		DropdownMenuLabel,
 		DropdownMenuGroup
-	} from '@aphex/ui/shadcn/dropdown-menu';
+	} from '@aphexcms/ui/shadcn/dropdown-menu';
 	import { Ellipsis } from '@lucide/svelte';
 
 	interface Props {

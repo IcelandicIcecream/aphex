b6623bec2ad2dff6ffe6e6445950a8e62552ba5a
Ben <benjaminsinidol@gmail.com>
2025-11-09 12:12:44 +0800

    added localapi interfaces, filters and postgres adapter support for localapi

--- packages/cms-core/src/lib/local-api/index.ts
+++ packages/cms-core/src/lib/local-api/index.ts
@@ -13,6 +13,7 @@ import type { LocalAPIContext } from './types';
  * Collections map - provides type-safe access to all collections
  * This interface is meant to be augmented by generated types
  */
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface Collections {
 	// This will be extended by module augmentation in generated-types.ts
 	// e.g., page: CollectionAPI<Page>, catalog: CollectionAPI<Catalog>, etc.

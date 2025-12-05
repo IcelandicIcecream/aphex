b0763b53d75c6d833355812d2428968a8ba6950d
Mohammed Alamri <modev32@gmail.com>
2025-12-06 02:31:13 +0300

    reset and correct array name

--- packages/cms-core/src/lib/utils/logger.ts
+++ packages/cms-core/src/lib/utils/logger.ts
@@ -1,15 +1,15 @@
-const EXECLUDE = [
-          'validateField',
-          'SchemaField.performValidation',
-          'Layout Calc',
-          'URL Effect',
-          'Document Editor'
+const EXCLUDE: string[] = [
+          // 'validateField',
+          // 'SchemaField.performValidation',
+          // 'Layout Calc',
+          // // 'URL Effect',
+          // 'Document Editor'
 ];
 
 export function cmsLogger(...args: any[]) {
           if (args.length > 0 && typeof args[0] === 'string') {
                     const title = args[0].toString().replace(/\[|\]/g, '');
-                    if (EXECLUDE.includes(title)) return;
+                    if (EXCLUDE.includes(title)) return;
                     console.log(...args);
           }
 }

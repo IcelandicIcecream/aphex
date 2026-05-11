1eea32e3b20c98a6d8330fdcfbb960d1f5f38319
Ben <benjaminsinidol@gmail.com>
2026-05-12 01:14:04 +0800

    update eslint config

--- eslint.config.js
+++ eslint.config.js
@@ -21,9 +21,23 @@ export default defineConfig(
 			globals: { ...globals.browser, ...globals.node }
 		},
 		rules: {
-			// typescript-eslint strongly recommend that you do not use the no-undef lint rule on TypeScript projects.
-			// see: https://typescript-eslint.io/troubleshooting/faqs/eslint/#i-get-errors-from-the-no-undef-rule-about-global-variables-not-being-defined-even-though-there-are-no-typescript-errors
-			'no-undef': 'off'
+			'no-undef': 'off',
+			'@typescript-eslint/no-explicit-any': 'off',
+			'@typescript-eslint/no-unused-vars': [
+				'warn',
+				{ argsIgnorePattern: '^_', varsIgnorePattern: '^_' }
+			],
+			'@typescript-eslint/ban-ts-comment': 'off',
+			'@typescript-eslint/no-unsafe-function-type': 'warn',
+			'@typescript-eslint/no-namespace': 'off',
+			'svelte/no-navigation-without-resolve': 'off',
+			'svelte/no-at-html-tags': 'warn',
+			'svelte/require-each-key': 'warn',
+			'svelte/prefer-svelte-reactivity': 'warn',
+			'svelte/prefer-writable-derived': 'warn',
+			'no-useless-catch': 'warn',
+			'no-useless-escape': 'warn',
+			'no-empty': 'warn'
 		}
 	},
 	{
